3946ee72552d610a4b1c4bb9cd0dd7a73732f628
salma erraji <82603945+salmaerraji@users.noreply.github.com>
2024-02-28 00:05:06 +0100

    Affichage des erreurs pour le username et password au moment de connexion

--- src/main/java/com/adrialma/controller/Login.java
+++ src/main/java/com/adrialma/controller/Login.java
@@ -16,23 +16,23 @@ import com.adrialma.form.LoginForm;
 @WebServlet("/Login")
 public class Login extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public Login() {
-        super();
-    }
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public Login() {
+		super();
+	}
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
+
 		//Tester la conection a la BD
 		//DaoBd.conecter();
 		//DaoBd.closeConnection();
-		
+
 		// Rediriger vers la page de login
 		this.getServletContext().getRequestDispatcher("/WEB-INF/Login.jsp").
 		forward(request, response);
@@ -42,25 +42,27 @@ public class Login extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		 // Traiter les informations du formulaire de login
-        LoginForm loginForm = new LoginForm(request);
+		// Traiter les informations du formulaire de login
+		LoginForm loginForm = new LoginForm(request);
 
-        if (loginForm.getErrorList().isEmpty()) {
-            // Connexion réussie
-            HttpSession session = request.getSession();
-            session.setAttribute("user", loginForm.getUser()); // Stocker l'utilisateur dans la session
-            //response.sendRedirect("/WEB-INF/HomePage.jsp"); // Rediriger vers la page d'accueil TODO*******
-            
-            this.getServletContext().getRequestDispatcher("/WEB-INF/HomePage.jsp").
-    		forward(request, response);
-           
-        } else {
-            // Connexion échouée
-            request.setAttribute("errors", loginForm.getErrorList()); // Passer la liste des erreurs
-            request.setAttribute("form", loginForm);
-            this.getServletContext().getRequestDispatcher("/WEB-INF/Login.jsp").forward(request, response); // Re-afficher la page de login avec les erreurs
-        }
-    }
+		if (loginForm.getErrorList().isEmpty()) {
+			// Connexion réussie
+			HttpSession session = request.getSession();
+			session.setAttribute("user", loginForm.getUser()); // Stocker l'utilisateur dans la session
+
+			// Rediriger vers la page d'accueil 
+			this.getServletContext().getRequestDispatcher("/WEB-INF/HomePage.jsp").
+			forward(request, response);
+
+		} else {
+			// Connexion échouée, erreurs dans les donnes, afficher la page d'erreur
+			request.setAttribute("errorList", loginForm.getErrorList());
+
+			// Re-afficher la page de login avec les erreurs
+			this.getServletContext().getRequestDispatcher("/WEB-INF/Login.jsp").forward(request, response); 
+
+		}
+	}
 
 }
 
